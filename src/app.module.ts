20923484bb26a0551d57e52a212dca3f930290b3
FernandoM52 <fernandomartins.contato@outlook.com>
2023-08-26 18:54:30 -0300

    chore/ Project setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { PostsModule } from './posts/posts.module';
+import { MediasModule } from './medias/medias.module';
+import { PublicationsModule } from './publications/publications.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, PostsModule, MediasModule, PublicationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,22 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { PrismaModule } from '../src/prisma/prisma.module';
+import { PrismaService } from '../src/prisma/prisma.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
+  let server;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, PrismaModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
+
+    prisma = await moduleFixture.resolve(PrismaService);
+    //TODO: fazer função cleanDB para limpar o banco de testes antes de cada execução
+
     await app.init();
+    server = app.getHttpServer();
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
+    return request(server)
       .get('/')
       .expect(200)
       .expect('Hello World!');
